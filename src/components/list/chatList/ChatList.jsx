57c029fcb3aba44faf2cf800ac635b43df94d09d
Kirsten Tran <uyen.tran@usm.edu>
2024-05-31 00:28:29 -0500

    30:48

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,6 +1,8 @@
 import "./chatList.css"
+import { useState } from "react"
 
-const ChatList= () => {
+const ChatList = () => {
+    const [addMode, setAddMode] = useState(false);
 return (
 <div className ='chatList' >
     <div className ='search'>
@@ -8,8 +10,44 @@ return (
             <img src="/search.png" alt=""/>
             <input type='text' placeholder="Search"/>
         </div>
-        <img src="./plus.png" alt ="" className="add"/>
-    </div>
+            <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className="add"
+                onClick={() => setAddMode((prev) => !prev)} />
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Uyen N. P. Tran</span>
+                <p> Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Uyen N. P. Tran</span>
+                <p> Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Uyen N. P. Tran</span>
+                <p> Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Uyen N. P. Tran</span>
+                <p> Hello</p>
+            </div>
+        </div>
+        <div className="item">
+            <img src="./avatar.png" alt="" />
+            <div className="texts">
+                <span>Uyen N. P. Tran</span>
+                <p> Hello</p>
+            </div>
+        </div>
 </div>
 )
 }

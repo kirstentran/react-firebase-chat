bfdf0c423dac8c2f7f08d33ab31e7f65cc18a5db
Kirsten Tran <uyen.tran@usm.edu>
2024-06-01 01:14:02 -0500

    34:22

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -2,9 +2,26 @@ import "./Chat.css"
 
 const Chat = () => {
 return (
-<div className ='chat'>Chat</div>
+<div className='chat'>
+            <div className="top">
+                  <div className="user">
+                        <img src="./avatar.png" alt="" />
+                        <div className="text">
+                              <span>Uyen N. P. Tran</span>
+                              <p>User Description</p>
+                        </div>
+                  </div>
+                  <div className="icons">
+                        <img src="./phone.png" alt="" />
+                        <img src="./video.png" alt="" />
+                        <img src="./info.png" alt="" />
+                  </div>            
+      
+</div>     
+            <div className ="center"></div>            
+            <div className ="bottom"></div>            
+</div>
 )
-
 }
 
 export default Chat

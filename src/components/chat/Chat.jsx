97cd8f9782df6e36645b27cce897d18d86e5ee9d
Kirsten Tran <uyen.tran@usm.edu>
2024-06-03 16:45:13 -0500

    1hr

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,6 +1,17 @@
-import "./Chat.css"
+import "./chat.css"
+import { useState } from "react"
+import EmojiPicker from "emoji-picker-react"
 
 const Chat = () => {
+      const [open, setOpen] = useState(false);
+      const [text, setText] = useState("");
+
+      const handleEmoji = e => {
+            setText(prev => prev + e.emoji);
+            setOpen (false)
+      };
+
+      console.log(text)
 return (
 <div className='chat'>
             <div className="top">
@@ -18,16 +29,76 @@ return (
                   </div>            
       
 </div>     
-      <div className ="center"></div>            
+            <div className="center">
+                  <div className="message">
+                        <img src="./avatar.png" alt="" />
+                        <div className="texts">
+                              <p>Random Text</p>
+                              <span>1 min ago</span>
+                        </div>
+                  </div>
+                  <div className="message own">
+                        <div className="texts">
+                              <p>Random Text</p>
+                              <span>1 min ago</span>
+                        </div>
+                  </div>
+                  <div className="message">
+                        <img src="./avatar.png" alt="" />
+                        <div className="texts">
+                              <p>Random Text</p>
+                              <span>1 min ago</span>
+                        </div>
+                  </div>
+                  <div className="message own">
+                        <div className="texts">
+                              <p>Random Text</p>
+                              <span>1 min ago</span>
+                        </div>
+                  </div>
+                  <div className="message">
+                        <img src="./avatar.png" alt="" />
+                        <div className="texts">
+                              <p>Random Text</p>
+                              <span>1 min ago</span>
+                        </div>
+                  </div>
+                  <div className="message own">
+                        <div className="texts">
+                              <p>Random Text</p>
+                              <span>1 min ago</span>
+                        </div>
+                  </div>
+                  <div className="message">
+                        <img src="./avatar.png" alt="" />
+                        <div className="texts">
+                              <p>Random Text</p>
+                              <span>1 min ago</span>
+                        </div>
+                  </div>
+                  <div className="message own">
+                        <div className="texts">
+                              <img src="https://p16-pu-sign-useast8.tiktokcdn-us.com/tos-useast5-avt-0068-tx/7363727724084953131~c5_100x100.jpeg?lk3s=a5d48078&nonce=85682&refresh_token=fff902ac0ee07b9a8b4067be1c683668&x-expires=1717610400&x-signature=8agllNeuJIv41QNgbTshOQvP7L4%3D&shp=a5d48078&shcp=81f88b70" alt="" />
+                              <p>Random Text</p>
+                              <span>1 min ago</span>
+                        </div>
+                  </div>
+            </div>            
             <div className="bottom">
                   <div className="icons">
                         <img src="./img.png" alt="" />
                         <img src="./camera.png" alt="" />
                         <img src="./mic.png" alt="" />
                   </div>
-                  <input type="text" placeholder="Type a message..." />
+                  <input type="text"
+                         placeholder="Type a message..."
+                         value = {text}
+                         onChange={e => setText(e.target.value)} />
                   <div className="emoji">
-                        <img src="./emoji.png" alt="" />
+                        <img src="./emoji.png" alt="" onClick={() => setOpen(prev =>!prev)} />
+                        <div className="picker">
+                        <EmojiPicker open={open} onEmojiClick={handleEmoji} />
+                        </div>
                   </div>
                   <button className="sendButton">Send</button>
             
--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,9 +1,73 @@
-import "./Detail.css"
+import "./detail.css"
 
 const Detail = () => {
-return (
-<div className ='detail' >Detail</div>
-)
-}
+      return (
+            <div className='detail' >
+                  <div className="user">
+                        <img src="./avatar.png" alt="" />
+                        <h2>Uyen N. P. Tran</h2>
+                        <p>User Dexcription</p>
+                  </div>
+                  <div className="info">
+                        <div className="option">
+                              <div className="title">
+                                    <span>Chat Settings</span>
+                                    <img src="./arrowUp.png" alt="" />
+                              </div>
+                        </div>
+                        <div className="option">
+                              <div className="title">
+                                    <span>Privacy and Help</span>
+                                    <img src="./arrowUp.png" alt="" />
+                              </div>
+                        </div>
+                        <div className="option">
+                              <div className="title">
+                                    <span>Shared Photos</span>
+                                    <img src="./arrowDown.png" alt="" />
+                              </div>
+                              <div className="photos">
+                                    <div className="photoItem">
+                                          <div className="photoDetail">
+                                                <img src="https://p16-pu-sign-useast8.tiktokcdn-us.com/tos-useast5-avt-0068-tx/7363727724084953131~c5_100x100.jpeg?lk3s=a5d48078&nonce=85682&refresh_token=fff902ac0ee07b9a8b4067be1c683668&x-expires=1717610400&x-signature=8agllNeuJIv41QNgbTshOQvP7L4%3D&shp=a5d48078&shcp=81f88b70" alt="" />
+                                                <span>photo_2024_2.png</span>
+                                          </div>
+                                          <img src="./download.png" alt="" className=" icon" />
+                                    </div>
+                                    <div className="photoItem">
+                                          <div className="photoDetail">
+                                                <img src="https://p16-pu-sign-useast8.tiktokcdn-us.com/tos-useast5-avt-0068-tx/7363727724084953131~c5_100x100.jpeg?lk3s=a5d48078&nonce=85682&refresh_token=fff902ac0ee07b9a8b4067be1c683668&x-expires=1717610400&x-signature=8agllNeuJIv41QNgbTshOQvP7L4%3D&shp=a5d48078&shcp=81f88b70" alt="" />
+                                                <span>photo_2024_2.png</span>
+                                          </div>
+                                          <img src="./download.png" alt="" className=" icon" />
+                                    </div>
+                                    <div className="photoItem">
+                                          <div className="photoDetail">
+                                                <img src="https://p16-pu-sign-useast8.tiktokcdn-us.com/tos-useast5-avt-0068-tx/7363727724084953131~c5_100x100.jpeg?lk3s=a5d48078&nonce=85682&refresh_token=fff902ac0ee07b9a8b4067be1c683668&x-expires=1717610400&x-signature=8agllNeuJIv41QNgbTshOQvP7L4%3D&shp=a5d48078&shcp=81f88b70" alt="" />
+                                                <span>photo_2024_2.png</span>
+                                          </div>
+                                          <img src="./download.png" alt="" className=" icon" />
+                                    </div>
+                                    <div className="photoItem">
+                                          <div className="photoDetail">
+                                                <img src="https://p16-pu-sign-useast8.tiktokcdn-us.com/tos-useast5-avt-0068-tx/7363727724084953131~c5_100x100.jpeg?lk3s=a5d48078&nonce=85682&refresh_token=fff902ac0ee07b9a8b4067be1c683668&x-expires=1717610400&x-signature=8agllNeuJIv41QNgbTshOQvP7L4%3D&shp=a5d48078&shcp=81f88b70" alt="" />
+                                                <span>photo_2024_2.png</span>
+                                          </div>
+                                          <img src="./download.png" alt="" className=" icon" />
+                                    </div>
+                              </div>
+                        </div>
+                  
+                        <div className="option">
+                              <div className="title">
+                                    <span>Shared Files</span>
+                                    <img src="./arrowUp.png" alt="" />
+                              </div>
+                        </div>
+                        <button>Block User</button>
+                  </div>  
+            </div>
+      );
+};
 
 export default Detail
